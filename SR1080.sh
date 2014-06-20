3a0d4d196e895d458b9fab9c258dfabdd57f9e45
Abdullah Alomair <Crunch3x@users.noreply.github.com>
2014-06-20 18:45:24 +0000

    Update SR1080.sh

--- SR1080.sh
+++ SR1080.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo cvt 1920 1080 60
-sudo xrandr --newmode "1920x1080_60.00" 173.00 1920 2048 2248 2576 1080 1083 10$
+sudo xrandr --newmode "1920x1080_60.00" 173.00 1920 2048 2248 2576 1080 1083 1088 1120 -hsync +vsync
 sudo xrandr -q
 for screen in $(sudo xrandr -q| grep " connected" | cut -d " " -f 1); do
 sudo xrandr --addmode $screen 1920x1080_60.00
